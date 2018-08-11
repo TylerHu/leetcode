1b382681fb3e9fb1679ff46ebfaed8b3719766a0
tyler <zhnhu@foxmail.com>
2018-08-11 17:43:39 +0800

    add another solution to leetcode 62

--- dp/UniquePath.java
+++ dp/UniquePath.java
@@ -11,6 +11,10 @@ package dp;
  * Obviously, dp[0][0] is 1, and the points in the first row and first column all have only one way to get there.
  * To point(i,j), there are two ways to get there, from the left point(i,j-1) and remove one step right, or from
  * the upper point(i-1, j) move one step down. So the dp formula is: dp[i][j] = dp[i-1][j] + dp[i][j-1];
+ *
+ * Another Solution: This problem can also be solved by using math formula.
+ * There are j = m + n - 2 steps to get to the finishing point, and k = m - 1 steps to get right to get to the finishing point.
+ * So the Combination(j,k) = j!/k!(j-k)! = (j-k+1)(j-k+2)*..*j/k!
  */
 public class UniquePath {
 
@@ -32,4 +36,16 @@ public class UniquePath {
         }
         return dp[m-1][n-1];
     }
+
+
+    public int uniquePaths2(int m, int n) {
+        int j = m + n - 2; // total steps to get to the end
+        int k = m - 1; // right steps to get to the end
+        double res = 1;
+        for (int i = 1; i <= k;  ++i) {
+            res = res * (j-k+i) / i;
+        }
+        return (int) res;
+    }
+
 }
