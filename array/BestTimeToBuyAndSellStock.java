16061118997e1d151859eec14f29b58372da241f
TylerHu <zhnhu@foxmail.com>
2018-08-17 19:45:27 +0800

    add another solution to leetcode 121

--- array/BestTimeToBuyAndSellStock.java
+++ array/BestTimeToBuyAndSellStock.java
@@ -7,20 +7,40 @@ package array;
  * If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
  * Note that you cannot sell a stock before you buy one.
  *
- * Solution: Keep track of the minimal price when iterating the array, and calcuate the profit when selling at each price. 
+ * Solution: Keep track of the minimal price when iterating the array, and calcuate the profit if sell the stock at price[i].
+ *
+ * Another solution: Use Kadane's Algorithm. Finding a continguous subarry giving maximum profit. If the current profit so far falls below zero,
+ * reset it to zero
  */
 public class BestTimeToBuyAndSellStock {
-    if (prices.length == 0) {
-        return 0;
+
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0) {
+            return 0;
+        }
+        //init the minPrice to first day price
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; ++i) {
+            //calculate the profit if sell the stock at price[i], and update the maxProfit
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+            //update the minPrice so far
+            minPrice = Math.min(minPrice, prices[i]);
+        }
+        return maxProfit;
     }
-    //init the minPrice to first day price
-    int minPrice = prices[0];
-    int maxProfit = 0;
-    for (int i = 1; i < prices.length; ++i) {
-        //calculate the profit if sell the stock at price[i], and update the maxProfit
-        maxProfit = Math.max(maxProfit, prices[i] - minPrice);
-        //update the minPrice so far
-        minPrice = Math.min(minPrice, prices[i]);
+
+    public int maxProfit2(int[] prices) {
+        if (prices.length == 0) {
+            return 0;
+        }
+        int profitSofar = 0;
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; ++i)
+            //calculate profit so far, it falls below zero, reset it to zero
+            profitSofar = Math.max(0, profitSofar += prices[i] - prices[i-1]);
+            maxProfit = Math.max(maxProfit, profitSofar);
+        }
+        return maxProfit;
     }
-    return maxProfit;
 }
