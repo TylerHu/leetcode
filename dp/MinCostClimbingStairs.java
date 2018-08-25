ccf2bb4b21c023c93ec2f226d8b3e10914d1e623
tyler <zhnhu@foxmail.com>
2018-08-25 16:45:03 +0800

    reduce space complexity

--- dp/MinCostClimbingStairs.java
+++ dp/MinCostClimbingStairs.java
@@ -9,6 +9,9 @@ package dp;
  *
  * Solution: Use dp approach. For every step(except 0th, 1th), can be reached from last step or last two steps, so the dp formula
  * is : dp[i] = cost[i] + Math.min(dp[i-1], dp[i-1]). The min cost to reach the top of the stairs is min(dp[n-1], dp[n-2])
+ *
+ * Since we only need to know the cost of last step dp[i-1], and the cost of last two steps dp[i-2], we can define two variables
+ * f0 and f1 to reduce the space complexity from O(n) to O(1).
  */
 public class MinCostClimbingStairs {
     public int minCostClimbingStairs(int[] cost) {
@@ -24,5 +27,19 @@ public class MinCostClimbingStairs {
         }
         //the min cost to reach the top of the stairs
         return Math.min(dp[n-1], dp[n-2]);
+    }
+
+    public int minCostClimbingStairs2(int[] cost) {
+        int n = cost.length;
+        int f0 = cost[0];
+        int f1 = cost[1];
+        int f2 = 0;
+
+        for (int i = 2; i < cost.length; ++i) {
+            f2 = cost[i] + Math.min(f0, f1);
+            f0 = f1;
+            f1 = f2;
+        }
+        return Math.min(f0, f1);
     }
 }
