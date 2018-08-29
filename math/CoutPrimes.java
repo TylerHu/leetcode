193b41233872a0e3e0d6d1ee63ab50318e8455f9
TylerHu <zhnhu@foxmail.com>
2018-08-29 14:54:17 +0800

    add solution to leetcode 204

---
+++ math/CoutPrimes.java
@@ -0,0 +1,28 @@
+package math;
+
+/**
+ * LeetCode204. Count Primes(https://leetcode.com/problems/count-primes/description/)
+ *
+ * Count the number of prime numbers less than a non-negative number, n.
+ *
+ * Solution: The idea is when number i is a prime number, then number i*j which i*j<n is not a prime number, use a array
+ * to store the state of all the numbers that less than n, and count how many numbers's state is false.
+ */
+public class CountPrimes {
+    public int countPrimes(int n) {
+        boolean[] notPrime = new boolean[n+1];
+        int count = 0;
+        for (int i = 2; i < n; ++i) {
+            if (!notPrime[i]) {
+                //number i is not a prime number, increase count
+                count++;
+                for (int j = 2; i*j < n; ++j) {
+                    //mark number i*j is prime
+                    notPrime[i*j] = true;
+                }
+            }
+        }
+        return count;
+
+    }
+}
