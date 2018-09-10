04699e72a6d05cd3d01eb66325eb1dd8d04b29f9
tyler <zhnhu@foxmail.com>
2018-09-10 23:38:07 +0800

    add iterative approach for postorder traversal

--- tree/BinaryTreePostorderTraversal.java
+++ tree/BinaryTreePostorderTraversal.java
@@ -6,7 +6,8 @@ package tree;
  * Given a binary tree, return the postorder traversal of its nodes' values.
  *
  * Solution: The basic idea is the same with leetcode 94. Postorder traversal means traverse the tree in left->right->root
- * order.
+ * order. For the iterative way, define a linked list instead of arraylist, since we need to addFirst method to
+ * add the value in a reverse order. And also push the right nodes. into the stack first, rather than left nodes.
  */
 public class BinaryTreePostorderTraversal {
     public List<Integer> preorderTraversal(TreeNode root) {
@@ -23,6 +24,26 @@ public class BinaryTreePostorderTraversal {
             }
             root = stack.pop();
             root = root.right;
+        }
+        return result;
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        //here we use linkedlist
+        LinkedList<Integer> result = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                //use addFirst method to add value in reverse order
+                result.addFirst(root.val);
+                stack.push(root);
+                root = root.right;
+            }
+            root = stack.pop();
+            root = root.left;
         }
         return result;
     }
