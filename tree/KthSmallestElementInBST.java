6fbddb6afa4f74bcd6792110367689cec6d6da8a
tyler <zhnhu@foxmail.com>
2018-09-12 23:20:40 +0800

    add recursive solution

--- tree/KthSmallestElementInBST.java
+++ tree/KthSmallestElementInBST.java
@@ -8,6 +8,7 @@ package tree;
  *
  * Solution: Use the same idea with leetcode98. Inorder traverse the binary search tree, so it's a sorted list in ascending
  * order, and return the kth element of the list.
+ * Also can do it in recursive way to traverse the tree in inorder and get the kth element.
  */
 public class KthSmallestElementInBST {
     public int kthSmallest(TreeNode root, int k) {
@@ -28,5 +29,27 @@ public class KthSmallestElementInBST {
             root = root.right;
         }
         return kthValue;
+    }
+
+    int count;
+    int num;
+    public int kthSmallest2(TreeNode root, int k) {
+        count = k;
+        inorder(root);
+        return num;
+    }
+
+    public void inorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        //minus iterating times
+        count--;
+        if (count == 0) {
+            //found it
+            num = root.val;
+        }
+        inorder(root.right);
     }
 }
