41d39efba2f4ee041f3268eabb829e724f7a7e47
TylerHu <zhnhu@foxmail.com>
2018-09-19 16:09:12 +0800

    add bucket sort solution

--- heap/TopKFrequentElements.java
+++ heap/TopKFrequentElements.java
@@ -8,6 +8,8 @@ package heap;
  * Solution: Use hash map to store appear times of each elements in the array. And define a new class FrequencyMap
  * to represent the number and it's appear times. Use priority queue to sort these frequencymaps according to their
  * frequncy value in reverse order. At the end, get the first k element of the priority queue, and put it into the result list.
+ *
+ * Another Solution is using bucket sort, put the number into corresponding bucket according to their appear times.
  */
 public class TopKFrequentElements {
     public List<Integer> topKFrequent(int[] nums, int k) {
@@ -44,5 +46,29 @@ public class TopKFrequentElements {
             //sort in reverse order
             return other.frequency - this.frequency;
         }
+    }
+
+    public List<Integer> topKFrequent2(int[] nums, int k) {
+        List<Integer> result = new ArrayList<>();
+        List<Integer>[] bucket = new ArrayList[nums.length+1];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int frequency = entry.getValue();
+            //use appear times as array index
+            if (bucket[frequency] == null) {
+                bucket[frequency] = new ArrayList<>();
+            }
+            bucket[frequency].add(entry.getKey());
+        }
+
+        for (int i = nums.length; i >= 0 && result.size() < k; --i) {
+            if (bucket[i] != null) {
+                result.addAll(bucket[i]);
+            }
+        }
+        return result;
     }
 }
